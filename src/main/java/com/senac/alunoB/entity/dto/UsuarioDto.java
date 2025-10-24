2763eb17f131a1e7bc57d63c6e62070111104fd9
dvaaz <darley.azevedo@gmail.com>
2025-10-24 12:03:13 -0300

    Commit inicial. Com gustavo

---
+++ src/main/java/com/senac/alunoB/entity/dto/UsuarioDto.java
@@ -0,0 +1,23 @@
+package com.senac.alunoB.entity.dto;
+
+public class UsuarioDto {
+    private String nome;
+    private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+}
